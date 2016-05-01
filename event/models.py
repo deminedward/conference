e7a0217620b3e6a93c30e9e4b38c7aa05970eaa8
deminedward <deminedward@gmail.com>
2016-05-01 18:19:13 +0800

    +createsuperuser without email

--- event/models.py
+++ event/models.py
@@ -36,21 +36,16 @@ class Event(models.Model):
 
 class MyUserManager(UserManager):
 
-    # def _create_user(self, username, password, **extra_fields):
-    #     if not username:
-    #         raise ValueError('The given username must be set')
-    #     user = self.model(username=username, **extra_fields)
-    #     user.set_password(password)
-    #     user.save(using=self._db)
-    #     return user
+
     #
     # def create_user(self, username, email=None, password=None, **extra_fields):
     #     extra_fields.setdefault('is_staff', False)
     #     extra_fields.setdefault('is_superuser', False)
     #     return self._create_user(username, password, **extra_fields)
 
-    def create_superuser(self, username,  password, **extra_fields):
-        u = self._create_user(username, password, email=None, **extra_fields)
+    def create_superuser(self, username,  password,  **extra_fields):
+        email = None
+        u = self._create_user(username, email, password,  **extra_fields)
         u.is_superuser = True
         u.is_staff = True
         u.save(using=self._db)
